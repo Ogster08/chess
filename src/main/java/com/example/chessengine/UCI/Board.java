f627b03e8400ac209582c1a014aee2d376ddd731
ogster <oliver.tate1@outlook.com>
2026-02-16 16:06:39 +0000

    Removed unused bit boards, added variable for which colour is currently moving, and updated constructor accordingly, while adding a new one, which defaults to white starting first, added lists for different move types, updated remove piece, with functionality going to the cell the piece is in, changed the move function to use the new move classes and accounts for resulting en passant moves from any double pawn moves, and finally a function to return all the pseudolegal moves in the current position by the current colour to move

--- src/main/java/com/example/chessengine/UCI/Board.java
+++ src/main/java/com/example/chessengine/UCI/Board.java
@@ -1,5 +1,9 @@
 package com.example.chessengine.UCI;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * The board class will hold the positions of all the pieces, and be able to add, move and remove them.
  * It will also generate all the legal moves in the current position
@@ -9,12 +13,38 @@ public class Board{
      * The array representation of the chessboard, where each cell can contain a piece
      */
     private final Cell[][] cells = new  Cell[8][8];
-    private long pieceBitBoard = 0;
-    private long whiteBitBoard = 0;
-    private long blackBitBoard = 0;
 
     /**
-     * Constructor to create a new empty board
+     * @return The current colour of which player's turn it is to move
+     */
+    public Colour getColourToMove() {
+        return colourToMove;
+    }
+
+    /**
+     * @param colourToMove The colour of what the current player's turn is being changed to
+     */
+    private void setColourToMove(Colour colourToMove) {
+        this.colourToMove = colourToMove;
+    }
+
+    /**
+     * Holds the current colour of which player's turn it is to move
+     */
+    private Colour colourToMove;
+
+    /**
+     * A list of all pseudolegal en passant moves in the current position, so they can easily be cleared once a move has been made
+     */
+    private final List<Move> enPassantMoves = new ArrayList<>();
+
+    /**
+     * A list of all the castling moves in the current position by the player whose turn it is, so they can more easily be validated if they are legal or not
+     */
+    private final List<CastlingMove> castlingMoves = new ArrayList<>();
+
+    /**
+     * Constructor to create a new empty board, where white starts first
      */
     public Board() {
         for(int i = 0; i < 8; i++){
@@ -22,6 +52,19 @@ public class Board{
                 cells[i][j] = new Cell(i, j);
             }
         }
+        colourToMove = Colour.WHITE;
+    }
+
+    /**
+     * Constructor to create a new empty board, choosing who goes first
+     */
+    public Board(Colour colourToMove) {
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                cells[i][j] = new Cell(i, j);
+            }
+        }
+        this.colourToMove = colourToMove;
     }
 
     /**
@@ -41,12 +84,6 @@ public class Board{
         int row = p.getRow();
         int col = p.getCol();
 
-        long mask = (1L << (col + 8 * row));
-        pieceBitBoard |= mask;
-
-        if (p.getColour() == Colour.WHITE){ whiteBitBoard |= mask; }
-        else if (p.getColour() == Colour.BLACK){ blackBitBoard |= mask; }
-
         cells[row][col].setPiece(p);
     }
 
@@ -54,45 +91,85 @@ public class Board{
      * @param p the piece to be removed
      */
     public void removePiece(Piece p){
-        int row = p.getRow();
-        int col = p.getCol();
-
-        long mask = ~(1L << (col + 8 * row));
-        pieceBitBoard &= mask;
-
-        if (p.getColour() == Colour.WHITE){ whiteBitBoard &= mask; }
-        else if (p.getColour() == Colour.BLACK){ blackBitBoard &= mask; }
-
-        p.removePiece();
-        cells[row][col].setPiece(null);
+        cells[p.getRow()][p.getCol()].setPiece(null);
     }
 
     /**
-     * moves a piece from one square to another using its position
-     * @param oldRow the row where it used to be (0 to 7 inclusive)
-     * @param oldCol the column where it used to be (0 to 7 inclusive)
-     * @param newRow the row where it is moving to (0 to 7 inclusive)
-     * @param newCol the column where it is moving to(0 to 7 inclusive)
+     * @param c the cell where the piece being removed is
      */
-    public void movePiece(int oldRow, int oldCol, int newRow, int newCol){
-        Piece p = cells[oldRow][oldCol].getPiece();
+    public void removePiece(Cell c){
+        c.setPiece(null);
+    }
 
-        long mask = (1L << (newCol + 8 * newCol));
-        pieceBitBoard |= mask;
+    /**
+     * Moves a piece from one square to another using its position.
+     * @param move The move containing the piece and cell it is moving to
+     */
+    public void movePiece(Move move){
+        enPassantMoves.clear();
+        Piece p = move.p();
 
-        if (p.getColour() == Colour.WHITE){ whiteBitBoard |= mask; }
-        else if (p.getColour() == Colour.BLACK){ blackBitBoard |= mask; }
+        if (p.getClass() == Pawn.class){
+            if (Math.abs(p.getRow() - move.cell().getRow()) == 2) {
+                if (move.cell().getCol() >= 1 && getCell(move.cell().getRow(), move.cell().getCol() - 1).getPiece() != null && getCell(move.cell().getRow(), move.cell().getCol() - 1).getPiece().getClass() == Pawn.class && getCell(move.cell().getRow(), move.cell().getCol() - 1).getPiece().getColour() != p.getColour()) {
+                    enPassantMoves.add(new EnPassantMove((Pawn) getCell(move.cell().getRow(), move.cell().getCol() - 1).getPiece(), getCell((move.cell().getRow() + move.p().getRow()) / 2, move.cell().getCol())));
+                }
+                if (move.cell().getCol() <= 6 && getCell(move.cell().getRow(), move.cell().getCol() + 1).getPiece() != null && getCell(move.cell().getRow(), move.cell().getCol() + 1).getPiece().getClass() == Pawn.class && getCell(move.cell().getRow(), move.cell().getCol() + 1).getPiece().getColour() != p.getColour()){
+                    enPassantMoves.add(new EnPassantMove((Pawn) getCell(move.cell().getRow(), move.cell().getCol() + 1).getPiece(), getCell((move.cell().getRow() + move.p().getRow()) / 2, move.cell().getCol())));
+                }
+            }
+        }
+        move.cell().setPiece(p);
+        cells[p.getRow()][p.getCol()].setPiece(null);
+        p.move(move.cell().getRow(), move.cell().getCol());
+        if (move.getClass() == EnPassantMove.class){
+            EnPassantMove enPassantMove = (EnPassantMove) move;
+            enPassantMove.targetPawnCell().setPiece(null);
+        } else if (move.getClass() == CastlingMove.class) {
+            CastlingMove castlingMove = (CastlingMove) move;
+            Rook rook = castlingMove.getR();
+            castlingMove.getRookCell().setPiece(rook);
+            cells[rook.getRow()][rook.getCol()].setPiece(null);
+            rook.move(castlingMove.getRookCell().getRow(), castlingMove.getRookCell().getCol());
+        }
+    }
 
-        cells[newRow][newCol].setPiece(p);
-
-        mask = ~(1L << (oldCol + 8 * oldRow));
-        pieceBitBoard &= mask;
-
-        if (p.getColour() == Colour.WHITE){ whiteBitBoard &= mask; }
-        else if (p.getColour() == Colour.BLACK){ blackBitBoard &= mask; }
-
-        cells[oldRow][oldCol].setPiece(null);
-
-        p.move(newRow, newCol);
+    /**
+     * Collates all the pseudolegal moves from the piece son the board that are the same colour as the current colour to move.
+     * It also adds all the castling moves into another list as well, so it is easier to check their legality later.
+     * @return The list of pseudolegal moves of the current colour to move on the board
+     */
+    public List<Move> getPseudolegalMoves() {
+        List<Move> moves = new ArrayList<>();
+        for (Cell[] row : cells) {
+            for (Cell cell : row) {
+                if (cell.getPiece() != null && cell.getPiece().getColour() == getColourToMove()) {
+                    moves.add(new Move(cell.getPiece(), cell));
+                    if (cell.getPiece().getClass() == King.class){
+                        King king = (King) cell.getPiece();
+                        if (king.canCastle) {
+                            Arrays.stream(cells).forEach(r -> {
+                                Arrays.stream(r).filter(c -> {
+                                    if (c.getPiece() != null && c.getPiece().getColour() == getColourToMove() && c.getPiece().getClass() == Rook.class){
+                                        Rook rook = (Rook) c.getPiece();
+                                        return rook.isCanCastle();
+                                    }
+                                    return false;
+                                }).forEach(c -> {
+                                    castlingMoves.add(new CastlingMove(king, (Rook) c.getPiece()));
+                                    moves.add(new CastlingMove(king, (Rook) c.getPiece()));
+                                });
+                            });
+                        }
+                    }
+                }
+            }
+        }
+        for (Move move : enPassantMoves){
+            if (move.p().getColour() == getColourToMove()){
+                moves.add(move);
+            }
+        }
+        return moves;
     }
 }
