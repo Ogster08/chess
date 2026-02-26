262e63ceef1d5ba3bcb05ff2c6d6a23fd05d73c9
ogster <oliver.tate1@outlook.com>
2026-02-26 08:41:05 +0000

    moved loading start position to Board class

--- src/main/java/com/example/chessengine/UCI/Board.java
+++ src/main/java/com/example/chessengine/UCI/Board.java
@@ -103,6 +103,40 @@ public class Board{
         zobristKey = zobrist.zobristKey(this);
     }
 
+    public static Board getStartPosition(){
+        Board board = new Board();
+        for (int col = 0; col < 8; col++) {
+            board.addPiece(new Pawn(board, 1, col, Colour.WHITE));
+        }
+        board.addPiece(new Rook(board, 0, 0, Colour.WHITE, true));
+        board.addPiece(new Rook(board, 0, 7, Colour.WHITE, true));
+
+        board.addPiece(new Knight(board, 0, 1, Colour.WHITE));
+        board.addPiece(new Knight(board, 0, 6, Colour.WHITE));
+
+        board.addPiece(new Bishop(board, 0, 2, Colour.WHITE));
+        board.addPiece(new Bishop(board, 0, 5, Colour.WHITE));
+
+        board.addPiece(new Queen(board, 0, 3, Colour.WHITE));
+        board.addPiece(new King(board, 0, 4, Colour.WHITE, true));
+
+        for (int col = 0; col < 8; col++) {
+            board.addPiece(new Pawn(board, 6, col, Colour.BLACK));
+        }
+        board.addPiece(new Rook(board, 7, 0, Colour.BLACK, true));
+        board.addPiece(new Rook(board, 7, 7, Colour.BLACK, true));
+
+        board.addPiece(new Knight(board, 7, 1, Colour.BLACK));
+        board.addPiece(new Knight(board, 7, 6, Colour.BLACK));
+
+        board.addPiece(new Bishop(board, 7, 2, Colour.BLACK));
+        board.addPiece(new Bishop(board, 7, 5, Colour.BLACK));
+
+        board.addPiece(new Queen(board, 7, 3, Colour.BLACK));
+        board.addPiece(new King(board, 7, 4, Colour.BLACK, true));
+        return board;
+    }
+
     /**
      * Moves a piece from one square to another using its position.
      * @param move The move containing the piece and cell it is moving to
