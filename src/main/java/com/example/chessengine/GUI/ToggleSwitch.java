ab0e9d3d2bb5d7a49306258393efb95b59dbc344
ogster <oliver.tate1@outlook.com>
2026-03-16 10:13:47 +0000

    improved toggle

--- src/main/java/com/example/chessengine/GUI/ToggleSwitch.java
+++ src/main/java/com/example/chessengine/GUI/ToggleSwitch.java
@@ -1,12 +1,17 @@
 package com.example.chessengine.GUI;
 
+import com.example.chessengine.Board.Pieces.King;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+
+import java.util.Objects;
 
 //Where this is based off of.
 //https://gist.github.com/TheItachiUchiha/12e40a6f3af6e1eb6f75
@@ -14,12 +19,12 @@ import javafx.scene.text.Font;
 /**
  * The ToggleSwitch class creates a toggle switch to be added to the page.
  */
-public class ToggleSwitch extends HBox {
+public class ToggleSwitch extends StackPane {
 
     /**
-     * The Label object for the text for the toggle switch
+     * The imageView object for the image for the toggle switch
      */
-    private final Label label = new Label();
+    private final ImageView imageView = new ImageView();
     /**
      * The Button object for the toggle switch
      */
@@ -36,19 +41,30 @@ public class ToggleSwitch extends HBox {
     public SimpleBooleanProperty switchOnProperty() { return switchedOn; }
 
     /**
-     * Initialises the toggle switch, by setting the default sate text, and adding
+     * The image of a white king to indicate the user is playing as white
+     */
+    Image white = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/white king.png")));
+
+    /**
+     * The image of a black king to indicate the user is playing as black
+     */
+    Image black = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/black king.png")));
+
+    /**
+     * Initialises the toggle switch, by setting the default state image, and events to  switch the state when clicked
      */
     private void init() {
 
-        label.setText("white");
+        imageView.setImage(white);
+        imageView.setPreserveRatio(true);
 
-        getChildren().addAll(label, button);
-        button.setOnAction((e) -> {
-            switchedOn.set(!switchedOn.get());
-        });
-        label.setOnMouseClicked((e) -> {
-            switchedOn.set(!switchedOn.get());
-        });
+        getChildren().addAll(imageView, button);
+        StackPane.setAlignment(imageView, Pos.CENTER_LEFT);
+        StackPane.setAlignment(button, Pos.CENTER_RIGHT);
+
+        button.setOnAction(e -> switchedOn.set(!switchedOn.get()));
+        imageView.setOnMouseClicked(e -> switchedOn.set(!switchedOn.get()));
+
         setStyle();
         bindProperties();
     }
@@ -59,19 +75,17 @@ public class ToggleSwitch extends HBox {
     private void setStyle() {
         //Default Width
         setWidth(80);
-        label.setAlignment(Pos.CENTER);
-        label.setFont(new Font(20));
         button.setStyle("-fx-background-radius: 0 100 100 0");
-        setStyle("-fx-background-color: white; -fx-background-radius: 100;");
+        setStyle("-fx-background-color: grey; -fx-background-radius: 100;");
         setAlignment(Pos.CENTER_LEFT);
     }
 
     /**
-     * Bind the layout of the label and button to this object, so they resize automatically
+     * Bind the layout of the imageView and button to this object, so they resize automatically
      */
     private void bindProperties() {
-        label.prefWidthProperty().bind(widthProperty().divide(2));
-        label.prefHeightProperty().bind(heightProperty());
+        imageView.fitWidthProperty().bind(widthProperty().divide(2));
+        imageView.fitHeightProperty().bind(heightProperty());
         button.prefWidthProperty().bind(widthProperty().divide(2));
         button.prefHeightProperty().bind(heightProperty());
     }
@@ -84,18 +98,18 @@ public class ToggleSwitch extends HBox {
         init();
         switchedOn.addListener((a,b,c) -> {
             if (c) {
-                label.setText("black");
-                label.setTextFill(Color.WHITE);
+                imageView.setImage(black);
+                StackPane.setAlignment(imageView, Pos.CENTER_RIGHT);
                 button.setStyle("-fx-background-radius:  100 0 0 100 ");
-                setStyle("-fx-background-color: black; -fx-background-radius: 100;");
-                label.toFront();
+                setStyle("-fx-background-color: grey; -fx-background-radius: 100;");
+                button.setTranslateX(-getWidth() / 2);
             }
             else {
-                label.setText("white");
-                label.setTextFill(Color.BLACK);
+                imageView.setImage(white);
+                StackPane.setAlignment(imageView, Pos.CENTER_LEFT);
                 button.setStyle("-fx-background-radius: 0 100 100 0");
-                setStyle("-fx-background-color: white; -fx-background-radius: 100;");
-                button.toFront();
+                setStyle("-fx-background-color: grey; -fx-background-radius: 100;");
+                button.setTranslateX(0);
             }
         });
     }
